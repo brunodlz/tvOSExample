34f0709e28416d1e4b71f0bfd9a23b3c4e22a46b
brunov0id <bruno@belab.com.br>
2015-10-16 10:05:14 -0300

    Initial Commit

---
+++ tvOSHelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tvOSHelloWorld
+//
+//  Created by Bruno da Luz on 10/16/15.
+//  Copyright © 2015 bruno v0id. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
